82caf1d7670ef3ea723797d6f7e488ded39406a9
Advel0 <0advel0@gmail.com>
2020-05-02 21:04:55 +0300

    'frond-end'

---
+++ application.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST', 'GET'])
+def search():
+    if request.method=='GET':
+        return 'Ay shalunishka!'
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username == 'Advel' and password == '123':
+        return render_template('search_page.html', username=username)
+    return 'TRY AGAIN'
+
+@app.route('/finded-books/', methods=['POST'])
+def find_book():
+    books=[]
+    book = request.form.get('book_name')
+    books.append(book)
+    return render_template('finded-books.html', books=books)
